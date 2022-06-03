43a3803e8536e6f505e58b5bd5c8e101eed1b807
josuecardenass <cacalaca@outlook.es>
2022-06-02 23:36:04 -0500

    josue cardenas

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+class Conectando{
+
+    private $con;
+
+    public function __construyendo()
+    {
+        $this->con = new mysqli('217.0.0.1', 'root', '', 'josuelogicas');
+    }       
+
+
+}
+
+?>
+
+
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    require('conexiones.php');
+?>
